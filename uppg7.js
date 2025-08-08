dfd9ecc6fe1bf85a50c565bc8dfe90100fc099ea
Munaabdullahi <Munaa.abdullahii@outlook.com>
2025-08-08 23:50:01 +0200

    Update uppg7.js

--- uppg7.js
+++ uppg7.js
@@ -5,8 +5,10 @@ function uppg7() {
     // skapa en funktion som tar in två tal som argument
     // funktionen ska returnera summan av talen
     // kalla på funktionen i en return statement och ge den parametrarna 5 och 10
-
- 
+function addNumbers(a, b) {
+   return a + b;
 }
-
+   return addNumbers(5, 10);
+}
+   
 module.exports = { uppg7 };
