fa5eb65e0b68bf6db49ddbb176e11df364b21506
Munaabdullahi <Munaa.abdullahii@outlook.com>
2025-08-08 23:41:32 +0200

    Update uppg5.js

--- uppg5.js
+++ uppg5.js
@@ -4,8 +4,14 @@ function uppg5() {
     // om det första talet är större än det andra, skriv ut "Det första talet är större än det andra"
     // annars skriv ut "Det andra talet är större än det första"
 
+var tal1 = 10;
+var tal2 = 1;
 
-
+    if (tal1 > tal2) {
+        console.log("Det första talet är större än det andra");
+    } else {
+        console.log("Det andra talet är större än det första");
+    }
 }
 
 module.exports = { uppg5 };
