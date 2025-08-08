6dc7c7e113b3991a56c7e810593a2b6ea54ed758
Munaabdullahi <Munaa.abdullahii@outlook.com>
2025-08-08 23:44:51 +0200

    Update uppg6.js

--- uppg6.js
+++ uppg6.js
@@ -4,8 +4,11 @@ function uppg6(){
 
     // Skapa en array med namn som du döper till names
     // Loopa igenom arrayen och skriv ut alla namn i konsolen
+var names = ["Abdi", "Adam", "Mustafa", "Johan"];
 
- 
+for (var i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
 }
 
 module.exports = { uppg6 };
