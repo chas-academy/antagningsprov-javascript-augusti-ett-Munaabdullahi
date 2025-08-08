c419f55ab6d97e9808f1ffe49319e058f9952739
Munaabdullahi <Munaa.abdullahii@outlook.com>
2025-08-08 23:53:19 +0200

    Update uppg8.js

--- uppg8.js
+++ uppg8.js
@@ -10,8 +10,21 @@ function uppg8(){
 //  över 30 år som skrivs ut i konsolen
 
 // anropa funktionen och skicka med arrayen som argument
-
-
+var people = [
+    { name: "Adam", age: 19 },
+    { name: "Stefan", age: 32 },
+    { name: "Abdi", age: 40 },
+    { name: "Maryan", age: 31 },
+    { name: "Salah", age: 35 }
+];
+function printOver30(arr) {
+  for (var i = 0; i < arr.length; i++) {
+        if (arr[i].age > 30) {
+            console.log(arr[i].name);
+        }
+    }
+}
+  printOver30(people);
 }
 
 module.exports = { uppg8 };
